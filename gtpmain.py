c3e7c6e60f42c312f7bf8855fa823b0e1a4c46ea
t-bonds <tjbonds98@gmail.com>
2020-11-21 13:42:29 -0600

    Updated Several Files to Python3

--- gtpmain.py
+++ gtpmain.py
@@ -14,7 +14,7 @@ import gtp.fraud
 import sigploit
 
 
-from gtp.gtp_v2_core.utilities.configuration_parser import parseConfigs
+from .gtp.gtp_v2_core.utilities.configuration_parser import parseConfigs
 
 config_file= ''
 remote_net =''
@@ -24,18 +24,18 @@ output_file ='results.csv'
 
 def gtpinfo():
     os.system('clear')
-    print " \033[31mInformation Gathering\033[0m ".center(105, "#")
-    print " \033[34mSelect an Attack from the below\033[0m ".center(105, "#")
-    print
-    print "   Attacks".rjust(10) + "\t\t\t\tDescription"
-    print "   --------                             ------------"
-    print "0) GTP Nodes Discovery".rjust(25) + "\t\tNE Discovery, using: EchoRequest,CreateSession,DeleteSession or DeleteBearer Messages"
-    print "1) TEID Allocation Discovery".rjust(31) + "\t\tTEID Discovery, using: CreateSession,ModifyBearer or CreateBearer Messages"
+    print(" \033[31mInformation Gathering\033[0m ".center(105, "#"))
+    print(" \033[34mSelect an Attack from the below\033[0m ".center(105, "#"))
+    print()
+    print("   Attacks".rjust(10) + "\t\t\t\tDescription")
+    print("   --------                             ------------")
+    print("0) GTP Nodes Discovery".rjust(25) + "\t\tNE Discovery, using: EchoRequest,CreateSession,DeleteSession or DeleteBearer Messages")
+    print("1) TEID Allocation Discovery".rjust(31) + "\t\tTEID Discovery, using: CreateSession,ModifyBearer or CreateBearer Messages")
 
-    print
-    print "or type back to go back to Attacks Menu".rjust(42)
+    print()
+    print("or type back to go back to Attacks Menu".rjust(42))
 
-    choice = raw_input("\033[37m(\033[0m\033[2;31minfo\033[0m\033[37m)>\033[0m ")
+    choice = input("\033[37m(\033[0m\033[2;31minfo\033[0m\033[37m)>\033[0m ")
 
     if choice == "0":
         gtp.info.nediscover()
@@ -44,30 +44,30 @@ def gtpinfo():
     elif choice == "back":
   		gtpattacksv2()
     else:
-        print '\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0-1)'
+        print('\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0-1)')
         time.sleep(1.5)
         gtpinfo()
 
 def gtpfraud():
     os.system('clear')
-    print " \033[31mFraud\033[0m ".center(105, "#")
-    print " \033[34mSelect an Attack from the below\033[0m ".center(105, "#")
-    print
-    print "   Attacks".rjust(10) + "\t\t\t\tDescription"
-    print "   --------                             ------------"
-    print "0) Tunnel Hijack".rjust(19) + "\t\tTEID Hijack, using: ModifyBearerRequest Message, TunnelHijack.cnf"
+    print(" \033[31mFraud\033[0m ".center(105, "#"))
+    print(" \033[34mSelect an Attack from the below\033[0m ".center(105, "#"))
+    print()
+    print("   Attacks".rjust(10) + "\t\t\t\tDescription")
+    print("   --------                             ------------")
+    print("0) Tunnel Hijack".rjust(19) + "\t\tTEID Hijack, using: ModifyBearerRequest Message, TunnelHijack.cnf")
 
-    print
-    print "or type back to go back to Attacks Menu".rjust(42)
+    print()
+    print("or type back to go back to Attacks Menu".rjust(42))
 
-    choice = raw_input("\033[37m(\033[0m\033[2;31minfo\033[0m\033[37m)>\033[0m ")
+    choice = input("\033[37m(\033[0m\033[2;31minfo\033[0m\033[37m)>\033[0m ")
 
     if choice == "0":
         gtp.fraud.thijack()
     elif choice == "back":
         gtpattacksv2()
     else:
-        print '\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0)'
+        print('\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0)')
         time.sleep(1.5)
         gtpfraud()
 
@@ -76,15 +76,15 @@ def gtpfraud():
 def gtpattacksv2():
     os.system('clear')
 
-    print " \033[34mChoose From the Below Attack Categories\033[0m ".center(105, "#")
-    print
-    print "0) Information Gathering".rjust(27)
-    print "1) Fraud".rjust(11)
-    print
-    print "or type back to return to the main menu".rjust(42)
-    print
+    print(" \033[34mChoose From the Below Attack Categories\033[0m ".center(105, "#"))
+    print()
+    print("0) Information Gathering".rjust(27))
+    print("1) Fraud".rjust(11))
+    print()
+    print("or type back to return to the main menu".rjust(42))
+    print()
 
-    choice = raw_input(
+    choice = input(
         "\033[37m(\033[0m\033[2;31mattacks\033[0m\033[37m)>\033[0m ")
 
     if choice == "0":
@@ -94,7 +94,7 @@ def gtpattacksv2():
     elif choice == 'back':
     	sigploit.mainMenu()
     else:
-        print '\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0-1)'
+        print('\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0-1)')
         time.sleep(1.5)
         gtpattacksv2()
 
@@ -102,12 +102,12 @@ def showOptions(config_file='', remote_net='', listening=True, verbosity=2, outp
 
     print('\n     Option                    \t\t\t\t\tValue')
     print('     --------                                                   ------')
-    print('     \033[34mconfig\033[0m     {:<15s} \t\t\t\033[31m%s\033[0m'.format('path to configuration file')) %config_file
-    print('     \033[34mtarget\033[0m     {:<15s} \t\033[31m%s\033[0m'.format('example: 10.10.10.1/32 or 10.10.10.0/24')) %remote_net
-    print('     \033[34mlistening\033[0m  {:<15s} \t\033[31m%s\033[0m'.format('accepting replies from target, default: True')) %listening
-    print('     \033[34mverbosity\033[0m  {:<15s} \t\t\t\033[31m%d\033[0m '.format('versbosity level, default: 2')) %verbosity
-    print('     \033[34moutput\033[0m     {:<15s} \t\t\033[31m%s\033[0m\n '.format('output file, default: result.csv')) %output_file
-   
+    print(('     \033[34mconfig\033[0m     {:<15s} \t\t\t\033[31m%s\033[0m'.format('path to configuration file')) %config_file)
+    print(('     \033[34mtarget\033[0m     {:<15s} \t\033[31m%s\033[0m'.format('example: 10.10.10.1/32 or 10.10.10.0/24')) %remote_net)
+    print(('     \033[34mlistening\033[0m  {:<15s} \t\033[31m%s\033[0m'.format('accepting replies from target, default: True')) %listening)
+    print(('     \033[34mverbosity\033[0m  {:<15s} \t\t\t\033[31m%d\033[0m '.format('versbosity level, default: 2')) %verbosity)
+    print(('     \033[34moutput\033[0m     {:<15s} \t\t\033[31m%s\033[0m\n '.format('output file, default: result.csv')) %output_file)
+
 
 def helpmenu():
 
@@ -119,22 +119,22 @@ def helpmenu():
     print("     \033[34mhelp\033[0m                        display this menu")
     print("     \033[34mback\033[0m                        back to GTP attacks")
     print("     \033[34mexit\033[0m                        exit SigPloit\n")
-  
+
 
 def prep():
-    print
-    print "   Module".rjust(10) + "\t\tDescription"
-    print "   --------             ------------"
-    print "0) GTPv1".rjust(8) + "\t\t3G Data attacks"
-    print "1) GTPv2".rjust(8) + "\t\t4G Data attacks"
-    print
-    print "or type back to go back to Main Menu".rjust(39)
+    print()
+    print("   Module".rjust(10) + "\t\tDescription")
+    print("   --------             ------------")
+    print("0) GTPv1".rjust(8) + "\t\t3G Data attacks")
+    print("1) GTPv2".rjust(8) + "\t\t4G Data attacks")
+    print()
+    print("or type back to go back to Main Menu".rjust(39))
 
-    choice = raw_input("\033[34mgtp\033[0m\033[37m>\033[0m ")
+    choice = input("\033[34mgtp\033[0m\033[37m>\033[0m ")
 
     if choice == "0":
-        print "\n\033[34m[*]\033[0mGTPv1 will be updated in version 2.1 release.."
-        print "\033[34m[*]\033[0mGoing back to GTP Menu"
+        print("\n\033[34m[*]\033[0mGTPv1 will be updated in version 2.1 release..")
+        print("\033[34m[*]\033[0mGoing back to GTP Menu")
         time.sleep(2)
         os.system('clear')
         prep()
@@ -144,4 +144,3 @@ def prep():
 
     elif choice == "back":
         sigploit.mainMenu()
-
--- ss7main.py
+++ ss7main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 '''
-SS7 main 
+SS7 main
 
 @author:     Loay Abdelrazek
 
@@ -27,21 +27,21 @@ def cleaner():
 def ss7tracking():
     os.system('clear')
 
-    print " \033[31mLocation Tracking\033[0m ".center(105, "#")
-    print " \033[34mSelect a Message from the below\033[0m ".center(105, "#")
-    print
-    print "   Message".rjust(10) + "\t\t\tDescription"
-    print "   --------                    ------------"
-    print "0) SendRoutingInfo".rjust(21) + "\t\tLocation Tracking, used to route calls could be blocked"
-    print "1) ProvideSubsriberInfo".rjust(26) + "\tReliable Location Tracking"
-    print "2) SendRoutingInfoForSM".rjust(26) + "\tReliable Location Tracking, if SMS home routing is not applied,should be run twice to check consistent replies"
-    print "3) AnyTimeInterrogation".rjust(26) + "\tLocation Tracking, blocked by most of operators"
-    print "4) SendRoutingInfoForGPRS".rjust(28) + "\tLocation tracking, used to route data, it will retrieve SGSN GT"
+    print(" \033[31mLocation Tracking\033[0m ".center(105, "#"))
+    print(" \033[34mSelect a Message from the below\033[0m ".center(105, "#"))
+    print()
+    print("   Message".rjust(10) + "\t\t\tDescription")
+    print("   --------                    ------------")
+    print("0) SendRoutingInfo".rjust(21) + "\t\tLocation Tracking, used to route calls could be blocked")
+    print("1) ProvideSubsriberInfo".rjust(26) + "\tReliable Location Tracking")
+    print("2) SendRoutingInfoForSM".rjust(26) + "\tReliable Location Tracking, if SMS home routing is not applied,should be run twice to check consistent replies")
+    print("3) AnyTimeInterrogation".rjust(26) + "\tLocation Tracking, blocked by most of operators")
+    print("4) SendRoutingInfoForGPRS".rjust(28) + "\tLocation tracking, used to route data, it will retrieve SGSN GT")
 
-    print
-    print "or type back to go back to Attacks Menu".rjust(42)
+    print()
+    print("or type back to go back to Attacks Menu".rjust(42))
 
-    choice = raw_input(
+    choice = input(
         "\033[37m(\033[0m\033[2;31mtracking\033[0m\033[37m)>\033[0m ")
 
     if choice == "0":
@@ -57,7 +57,7 @@ def ss7tracking():
     elif choice == "back":
         attacksMenu()
     else:
-        print '\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0 - 4)'
+        print('\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0 - 4)')
         time.sleep(1.5)
         ss7tracking()
 
@@ -65,17 +65,17 @@ def ss7tracking():
 def ss7interception():
     os.system('clear')
 
-    print " \033[31mInterception\033[0m ".center(105, "#")
-    print " \033[34mSelect a Message from the below\033[0m ".center(105, "#")
-    print
-    print "   Message".rjust(10) + "\t\t\t\tDescription"
-    print "   --------                             -----------"
-    print "0) UpdateLocation".rjust(20) + "\t\t\tStealthy SMS Interception"
+    print(" \033[31mInterception\033[0m ".center(105, "#"))
+    print(" \033[34mSelect a Message from the below\033[0m ".center(105, "#"))
+    print()
+    print("   Message".rjust(10) + "\t\t\t\tDescription")
+    print("   --------                             -----------")
+    print("0) UpdateLocation".rjust(20) + "\t\t\tStealthy SMS Interception")
 
-    print
-    print "or type back to go back to Attacks Menu".rjust(42)
+    print()
+    print("or type back to go back to Attacks Menu".rjust(42))
 
-    choice = raw_input(
+    choice = input(
         "\033[37m(\033[0m\033[2;31minterception\033[0m\033[37m)>\033[0m ")
 
     if choice == "0":
@@ -84,7 +84,7 @@ def ss7interception():
     elif choice == "back":
         attacksMenu()
     else:
-        print '\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0)'
+        print('\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0)')
         time.sleep(1.5)
         ss7interception()
 
@@ -92,20 +92,20 @@ def ss7interception():
 def ss7fraud():
     os.system('clear')
 
-    print " \033[31mFraud & Info\033[0m ".center(105, "#")
-    print " \033[34mSelect a Message from the below\033[0m ".center(105, "#")
-    print
-    print "   Message".rjust(10) + "\t\t\t\tDescription"
-    print "   --------                            ------------"
-    print "0) SendIMSI".rjust(14) + "\t\t\t\tRetrieving IMSI of a subscriber"
-    print "1) MTForwardSMS".rjust(18) + "\t\t\tSMS Phishing and Spoofing"
-    print "2) InsertSubscriberData".rjust(26) + "\t\tSubscriber Profile Maniuplation"
-    print "3) SendAuthenticationInfo".rjust(28) + "\t\tSubscriber Authentication Vectors retrieval"
+    print(" \033[31mFraud & Info\033[0m ".center(105, "#"))
+    print(" \033[34mSelect a Message from the below\033[0m ".center(105, "#"))
+    print()
+    print("   Message".rjust(10) + "\t\t\t\tDescription")
+    print("   --------                            ------------")
+    print("0) SendIMSI".rjust(14) + "\t\t\t\tRetrieving IMSI of a subscriber")
+    print("1) MTForwardSMS".rjust(18) + "\t\t\tSMS Phishing and Spoofing")
+    print("2) InsertSubscriberData".rjust(26) + "\t\tSubscriber Profile Maniuplation")
+    print("3) SendAuthenticationInfo".rjust(28) + "\t\tSubscriber Authentication Vectors retrieval")
 
-    print
-    print "or type back to go back to Attacks Menu".rjust(42)
+    print()
+    print("or type back to go back to Attacks Menu".rjust(42))
 
-    choice = raw_input(
+    choice = input(
         "\033[37m(\033[0m\033[2;31mfraud\033[0m\033[37m)>\033[0m ")
 
     if choice == "0":
@@ -119,7 +119,7 @@ def ss7fraud():
     elif choice == "back":
         attacksMenu()
     else:
-        print '\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0-3)'
+        print('\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0-3)')
         time.sleep(1.5)
         ss7fraud()
 
@@ -127,17 +127,17 @@ def ss7fraud():
 def ss7dos():
     os.system('clear')
 
-    print " \033[31mDenial of Service\033[0m ".center(105, "#")
-    print " \033[34mSelect a Message from the below\033[0m ".center(105, "#")
-    print
-    print "   Message".rjust(10) + "\t\t\t\tDescription"
-    print "   --------                            ------------"
-    print "0) PurgeMS-Subscriber DoS".rjust(28) + "\t\t Mass DoS attack on Subscribers to take them off network"
+    print(" \033[31mDenial of Service\033[0m ".center(105, "#"))
+    print(" \033[34mSelect a Message from the below\033[0m ".center(105, "#"))
+    print()
+    print("   Message".rjust(10) + "\t\t\t\tDescription")
+    print("   --------                            ------------")
+    print("0) PurgeMS-Subscriber DoS".rjust(28) + "\t\t Mass DoS attack on Subscribers to take them off network")
 
-    print
-    print "or type back to go back to Attacks Menu".rjust(42)
+    print()
+    print("or type back to go back to Attacks Menu".rjust(42))
 
-    choice = raw_input(
+    choice = input(
         "\033[37m(\033[0m\033[2;31mdos\033[0m\033[37m)>\033[0m ")
 
     if choice == "0":
@@ -145,7 +145,7 @@ def ss7dos():
     elif choice == "back":
         attacksMenu()
     else:
-        print '\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0)'
+        print('\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0)')
         time.sleep(1.5)
         ss7dos()
 
@@ -153,17 +153,17 @@ def ss7dos():
 def attacksMenu():
     os.system('clear')
 
-    print " \033[34mChoose From the Below Attack Categories\033[0m ".center(105, "#")
-    print
-    print "0) Location Tracking".rjust(23)
-    print "1) Call and SMS Interception".rjust(31)
-    print "2) Fraud & Info Gathering".rjust(28)
-    print "3) DoS".rjust(9)
-    print
-    print "or type back to return to the main menu".rjust(42)
-    print
+    print(" \033[34mChoose From the Below Attack Categories\033[0m ".center(105, "#"))
+    print()
+    print("0) Location Tracking".rjust(23))
+    print("1) Call and SMS Interception".rjust(31))
+    print("2) Fraud & Info Gathering".rjust(28))
+    print("3) DoS".rjust(9))
+    print()
+    print("or type back to return to the main menu".rjust(42))
+    print()
 
-    choice = raw_input(
+    choice = input(
         "\033[37m(\033[0m\033[2;31mattacks\033[0m\033[37m)>\033[0m ")
 
     if choice == "0":
@@ -181,6 +181,6 @@ def attacksMenu():
     elif choice == "back":
         sigploit.mainMenu()
     else:
-        print '\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0 - 3)'
+        print('\n\033[31m[-]Error:\033[0m Please Enter a Valid Choice (0 - 3)')
         time.sleep(1.5)
         attacksMenu()
